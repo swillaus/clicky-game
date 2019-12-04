cfee19aa2db7fda5a86acb216e2e86542e2669ca
Your Name <sam@upsidebusinessgroup.com.au>
2019-12-03 20:41:06 -0500

    Updates

---
+++ precache-manifest.761b3687ec42679265da715115166230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b91f0adae8108e3c22914a69dbf9d0c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b48d04a3d0c51744a091",
+    "url": "/clicky-game/static/css/main.845600be.chunk.css"
+  },
+  {
+    "revision": "0115222356d88c0414e0",
+    "url": "/clicky-game/static/js/2.50fb5cbd.chunk.js"
+  },
+  {
+    "revision": "b48d04a3d0c51744a091",
+    "url": "/clicky-game/static/js/main.c2044b2f.chunk.js"
+  },
+  {
+    "revision": "166d09415de4c52327d7",
+    "url": "/clicky-game/static/js/runtime-main.cf0d8eec.js"
+  }
+]);
